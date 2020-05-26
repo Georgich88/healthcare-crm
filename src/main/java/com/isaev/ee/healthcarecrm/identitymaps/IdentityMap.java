0579c8428bb00d5df4bf6c927c9d2ea7f37fe059
Georgich88 <georgeisaev@gmail.com>
2020-05-26 23:10:26 +0700

    #8 Add identity map comments

--- src/main/java/com/isaev/ee/healthcarecrm/identitymaps/IdentityMap.java
+++ src/main/java/com/isaev/ee/healthcarecrm/identitymaps/IdentityMap.java
@@ -1,11 +1,18 @@
 package com.isaev.ee.healthcarecrm.identitymaps;
 
-public interface IdentityMap<K,V> {
+/**
+ * @author Ensures that each object gets loaded only once by keeping every
+ *         loaded object in a map.
+ *
+ * @param <K> - the key for the map.
+ * @param <V> - the object for the map.
+ */
+public interface IdentityMap<K, V> {
 
 	void add(V value);
-	
+
 	void add(K key, V value);
-	
+
 	V get(K key);
-	
+
 }
